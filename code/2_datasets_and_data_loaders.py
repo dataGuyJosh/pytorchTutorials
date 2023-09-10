4e52d817ed74010a1e8dba0dfb5240d3371f2fae
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2023-09-10 19:25:48 +1000

    finished data loaders

--- code/2_datasets_and_data_loaders.py
+++ code/2_datasets_and_data_loaders.py
@@ -20,7 +20,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
-
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -34,3 +33,82 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+# iterating & visualizing datasets
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+
+# creating a custom dataset
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    # initialize the directory containing the images, the annotations file, and both transforms
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+    # the number of samples in the dataset
+    def __len__(self):
+        return len(self.img_labels)
+    # load and return a sample from the dataset at given index
+    def __getitem__(self, idx):
+        # identifies the image’s location on disk
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        # converts to a tensor
+        image = read_image(img_path)
+        # retrieves the corresponding label from the csv data in self.img_labels
+        label = self.img_labels.iloc[idx, 1]
+        # calls the transform functions on them (if applicable)
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        # returns the tensor image and corresponding label in a tuple
+        return image, label
+
+# preparing data for training
+'''
+The "Dataset" retrieves our dataset's features and labels one sample at a time.
+While training a model, we typically want to pass samples in "minibatches",
+reshuffle the data at every epoch to reduce model overfitting,
+and use Python's multiprocessing to speed up data retrieval.
+
+DataLoader is an iterable that abstracts this complexity for us in an easy API.
+'''
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# iterate through DataLoader --> display image and label
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
